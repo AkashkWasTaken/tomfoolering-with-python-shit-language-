2e4c34e5e59cfdeb30926c6dfe2a5c2d62c5a93c
Akashk <95849551+AkashkWasTaken@users.noreply.github.com>
2025-04-02 23:50:41 +0530

    Add files via upload

---
+++ circumference of circle.py
@@ -0,0 +1,4 @@
+import math
+radius = float(input("What is the radius of the circle? :"))
+circ = 2*math.pi*radius
+print(f"The circumference of the circle is: {round(circ,2)} ")
---
+++ hypotenuse.py
@@ -0,0 +1,5 @@
+import math
+a= float(input("Enter side A :"))
+b = float(input("Enter side B :"))
+c= math.sqrt(math.pow(a,2)+ math.pow(b,2))
+print(f"length of side c is: {(round(c,2))}")
