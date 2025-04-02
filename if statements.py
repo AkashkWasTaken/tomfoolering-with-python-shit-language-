dbe29450b87e45f4e0e4cb6a67aa0d06736820ca
Akashk <95849551+AkashkWasTaken@users.noreply.github.com>
2025-04-03 02:18:21 +0530

    Add files via upload

---
+++ if statements.py
@@ -0,0 +1,11 @@
+age = int(input("enter your age:"))
+if 18> age > 0:
+    print("You need to be 18+ to sign up")
+elif age <1:
+    print("jajajaja idiot")
+elif 100 >age > 17:
+    print("You are now signed up!")
+elif age > 100:
+    print("Die")
+else :
+    print("kys")
