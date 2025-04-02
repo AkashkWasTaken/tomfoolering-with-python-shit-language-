2f8325c7c2b035e781dda0869e329b7cc0d7408b
Akashk <95849551+AkashkWasTaken@users.noreply.github.com>
2025-04-03 02:31:50 +0530

    Add files via upload

---
+++ conditional expressions.py
@@ -0,0 +1,13 @@
+num = 5
+a= 6
+b=7
+age = 13
+user_role = "guest"
+#print("positive" if num > 0 else "negative")
+#print("even" if num %2 ==0 else "odd")
+#print("max_num = a if  a > b else b")
+#print("min_num = a if a<b else b")
+#print("Full access" if user_role =="Admin" else "Limited access")
+
+
+
